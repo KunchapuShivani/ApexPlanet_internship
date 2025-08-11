6baeb6b459d724868177d08842c89fd274c72640
Kunchapu Lakshmi Shivani <kunchapushivani2005@gmail.com>
2025-08-11 19:51:15 +0530

    Add files via upload
    Task-1 
    Basics of Web Development

---
+++ week - 1/web script.js
@@ -0,0 +1,7 @@
+// Selecting the button
+const alertButton = document.getElementById("alertBtn");
+
+// Adding click event listener
+alertButton.addEventListener("click", function() {
+    alert("Hello! You clicked the button.");
+});
